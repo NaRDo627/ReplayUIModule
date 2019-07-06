4e73f6524119225f8cd3004149ef85399e5c20d6
leechangkuk <huthealth@naver.com>
2019-07-07 01:09:29 +0900

    feat: pubg api 통신 test(#3)

--- src/main/java/com/threeCodeMonkeyz/ReplayUIModule/Controllers/APIController.java
+++ src/main/java/com/threeCodeMonkeyz/ReplayUIModule/Controllers/APIController.java
@@ -1,9 +1,13 @@
 package com.threeCodeMonkeyz.ReplayUIModule.Controllers;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.apache.commons.jcs.utils.zip.CompressionUtil;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
 
 @RestController
 @RequestMapping(value = "/api")
@@ -27,7 +31,7 @@ public class APIController {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add("Origin", "https://developer.riotgames.com");
         httpHeaders.add( "Accept-Charset", "application/x-www-form-urlencoded; charset=UTF-8");
-        httpHeaders.add("X-Riot-Token","RGAPI-b453fc8b-683f-4854-918f-a3edcc65fe52");
+        httpHeaders.add("X-Riot-Token","RGAPI-94b62fe9-6e88-4ecd-a26a-3d790a21ca29");
         httpHeaders.add("Accept-Language","ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7");
         httpHeaders.add("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36");
 
@@ -35,4 +39,40 @@ public class APIController {
 
         return restTemplate.exchange(url, HttpMethod.GET, new HttpEntity(httpHeaders), String.class);
     }
+
+    @RequestMapping(value ="/pubg/{platform}/{matchId}",method = RequestMethod.GET)
+    public ResponseEntity<String> getPubgData(@PathVariable("platform") String platform,@PathVariable("matchId") String matchId) throws Exception{
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Accept", "application/vnd.api+json");
+
+        String url = "https://api.pubg.com/shards/"+platform+"/matches/"+matchId;
+        ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET, new HttpEntity(httpHeaders), String.class);
+        String data = responseEntity.getBody();
+
+        JsonParser Parser = new JsonParser();
+        JsonArray dataArray = (JsonArray)Parser.parse(data).getAsJsonObject().get("data").getAsJsonObject().get("relationships").getAsJsonObject().get("assets").getAsJsonObject().get("data");
+        JsonObject dataObject = (JsonObject)dataArray.get(0);
+        String id = dataObject.get("id").toString();
+        System.out.println(id);
+        JsonArray includedArray = (JsonArray)Parser.parse(data).getAsJsonObject().get("included");
+        for(int i=0;i<includedArray.size();i++) {
+            JsonObject includedObject = (JsonObject) includedArray.get(i);
+            if(includedObject.get("id").toString().equals(id)) { //string null 체크?
+                JsonObject attrObject = (JsonObject) includedObject.get("attributes");
+                url = attrObject.get("URL").getAsString();
+                break;
+            }
+        }
+        System.out.println(url);
+
+       // int size = url.length();
+       // url = url.substring(1,size-1); //앞 뒤에 %22(")가 붙음 왜 그런진 모르겠음 (url을 toString으로 가져오면 생김,getAsString으로 해결)
+        //httpHeaders.add("Accept-Encoding","gzip"); gzip 안붙여도 왜 되지..?
+        byte[] telemetryGzip = restTemplate.exchange(url, HttpMethod.GET, new HttpEntity(httpHeaders), byte[].class).getBody();
+        //String  decompressed = null;
+        String telemetry= new String(CompressionUtil.decompressGzipByteArray(telemetryGzip));
+        JsonArray jsonArrayTelemetry = (JsonArray)Parser.parse(telemetry);
+        return new ResponseEntity<>(jsonArrayTelemetry.toString(), HttpStatus.OK);
+            }
 }
